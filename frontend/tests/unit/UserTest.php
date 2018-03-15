572d41b35e76d69dad5559a35e4119c901f32870
DarmaBoroev <dboroev@gmail.com>
2018-03-15 19:21:15 +0800

    Unit test for UnfollowUser method

--- frontend/tests/unit/UserTest.php
+++ frontend/tests/unit/UserTest.php
@@ -1,19 +1,21 @@
 <?php
+
 namespace frontend\tests;
+
 use frontend\tests\fixtures\UserFixture;
 use Yii;
 
-class UserTest extends \Codeception\Test\Unit
-{
+class UserTest extends \Codeception\Test\Unit {
+
     /**
      * @var \frontend\tests\UnitTester
      */
     protected $tester;
-    
-    public function _fixtures(){
+
+    public function _fixtures() {
         return ['users' => UserFixture::className()];
     }
-    
+
     public function _before() {
         Yii::$app->setComponents([
             'redis' => [
@@ -24,33 +26,48 @@ class UserTest extends \Codeception\Test\Unit
             ],
         ]);
     }
-    
-    public function testGetNicknameOnNicknameEmpty(){
+
+    public function testGetNicknameOnNicknameEmpty() {
         /* @var $user frontend\models\User */
         $user = $this->tester->grabFixture('users', 'user1');
         expect($user->getNickname())->equals(1);
     }
-    
-    public function testGetNicknameOnNicknameNotEmpty(){
+
+    public function testGetNicknameOnNicknameNotEmpty() {
         $user = $this->tester->grabFixture('users', 'user2');
         expect($user->getNickname())->equals('catelyn');
     }
-    
-    public function testGetPostCount(){
+
+    public function testGetPostCount() {
         $user = $this->tester->grabFixture('users', 'user1');
         expect($user->getPostCount())->equals(3);
     }
-    
-    public function testFollowUser(){
+
+    public function testFollowUser() {
         $user1 = $this->tester->grabFixture('users', 'user1');
         $user3 = $this->tester->grabFixture('users', 'user3');
-        
+
         $user3->followUser($user1);
-        
+
         $this->tester->seeRedisKeyContains('user:1:followers', 3);
         $this->tester->seeRedisKeyContains('user:3:subcribtions', 1);
-        
+
         $this->tester->sendCommandToRedis('del', 'user:1:followers');
         $this->tester->sendCommandToRedis('del', 'user:3:subcribtions');
     }
+
+    public function testUnfollowUser() {
+        $user1 = $this->tester->grabFixture('users', 'user1');
+        $user3 = $this->tester->grabFixture('users', 'user3');
+
+        $user3->followUser($user1);
+        $user3->unfollowUser($user1);
+
+        $this->tester->dontSeeInRedis('user:1:followers', 3);
+        $this->tester->dontSeeInRedis('user:3:subcribtions', 1);
+
+        $this->tester->sendCommandToRedis('del', 'user:1:followers');
+        $this->tester->sendCommandToRedis('del', 'user:3:subcribtions');
+    }
+
 }
